e3219d319092a5244a288bbaf1c66090f9f83251
AfricanMonk <34459397+AfricanMonk@users.noreply.github.com>
2018-04-16 09:19:53 -0300

    Update FL.java

--- FL.java
+++ FL.java
@@ -3,39 +3,39 @@ import java.io.IOException;
 
 public class FL extends Organiza{
 	private String tag; 
-	private String[][] FL=new String[Index.length][nway+1];//Matriz das tags
+	private String[][] FL=new String[Index.length][nway+1];
 	private int miss;
 	private int hit;
-	private String forma;//Se vai ser Lru ou Fifo
-	int cont=0;//contador para atender a uma condição
+	private String forma;
+	int cont=0;
 	int pivo=0;
 	
 	FL(File a, int b, int c,String f) throws IOException {
 		super(a, b, c);
 		this.forma=f;
-		for(int k=0; k<linha.size();k++) { //Irá roda ate o tamanho do arq
-			this.tag=conversorHB(k);//Tag recebe a da primeira linha do arq
-			cont=0; //Para verificar se o conjunto está cheio
-			for(int j=0;j<nway;j++) { // Percorre o tam do conjunto
-				if(FL[In][j]==null) { // Nas primeiras vezes ele será sempre nullo
-					this.FL[In][j]=tag; // Armazena a tag em sua matriz
-					this.FLaux[In][j]=j+1; // Salva a sua posição na matriz auxiliar
+		for(int k=0; k<linha.size();k++) { 
+			this.tag=conversorHB(k);
+			cont=0; 
+			for(int j=0;j<nway;j++) { 
+				if(FL[In][j]==null) { 
+					this.FL[In][j]=tag; 
+					this.FLaux[In][j]=j+1; 
 					this.miss++;
 					break;
 				}
-				int com=Integer.parseInt(FL[In][j], 2); //Converte
+				int com=Integer.parseInt(FL[In][j], 2); 
 				int para=Integer.parseInt(tag, 2);
-				if(com==para) { //"Compara"
-					if(forma=="LRU") { //Somente se o metodo for Lru
-						repaginar(forma,j);//atualiza
+				if(com==para) { 
+					if(forma=="LRU") { 
+						repaginar(forma,j);
 					}
-					this.hit++; //Se for == hit++
+					this.hit++; 
 					break;
 				}
 				else if(FL[In][j]!=null) {
-					if(cont==nway-1) { //Significa que o conjunto encheu
-						pivo=repaginar("Fifo",0); //Recebe a posição do menor
-						this.FL[In][pivo]=tag; // coloca a tag no menor
+					if(cont==nway-1) {
+						pivo=repaginar("Fifo",0); 
+						this.FL[In][pivo]=tag; 
 						this.miss++;
 					}
 					cont++;
