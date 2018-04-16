30d292b7c36c39bbf7a8302da38871415256987b
AfricanMonk <34459397+AfricanMonk@users.noreply.github.com>
2018-04-16 09:18:21 -0300

    Update Direto.java

--- Direto.java
+++ Direto.java
@@ -2,9 +2,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class Direto extends Organiza {
-	private String tag; //Recebe a Tag
+	private String tag;
 	private String[][] N=new String[Index.length][nway];
-	//Acertos e erros
 	private int miss;
 	private int hit;
 	
@@ -15,16 +14,16 @@ public class Direto extends Organiza {
 				N[k][j]="0";
 			}
 		}
-		for(int k=0; k<linha.size();k++) { //Percorre o numero de informa��es lidas
-			this.tag=conversorHB(k);//Pega a primeira Tag
+		for(int k=0; k<linha.size();k++) { 
+			this.tag=conversorHB(k);
 			int com=Integer.parseInt(N[In][0], 2); 
 			int para=Integer.parseInt(tag, 2);
 			if(com!=para) {
 				this.N[In][0]=tag;
-				this.miss++;//Se for != miss++	
+				this.miss++;
 			}
 			else {
-				this.hit++; //Se for == hit++
+				this.hit++; 
 			}	
 		}
 		System.out.println("Erros:"+miss);
